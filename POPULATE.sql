e235330bb4b91db961311b6a41ccec49dade2da6
DragonFlyer139 <40473280+DragonFlyer139@users.noreply.github.com>
2018-12-12 15:48:24 -0600

    Update POPULATE.sql to all lowercase

--- POPULATE.sql
+++ POPULATE.sql
@@ -1,143 +1,143 @@
-/*Not sure if I mentioned this before, but I'm using MariaDB
-Order of tables to populate:
-	STORE
-	EMPLOYEE
-	MOVIE
-	MEMBER
-	COPY
-	PLAYER
-	INVOICE_TRANSACTIONS
-		Typically these are created when we actually have invoices
-		But we're going to need an invoice history to test
-	NO_ASSIGNED
-	HWORKS
-	FULL_TIME
-	PLAYER_DEVICE
-	STORE_CHARGE
+/*not sure if i mentioned this before, but i'm using mariadb
+order of tables to populate:
+	store
+	employee
+	movie
+	member
+	copy
+	player
+	invoice_transactions
+		typically these are created when we actually have invoices
+		but we're going to need an invoice history to test
+	no_assigned
+	hworks
+	full_time
+	player_device
+	store_charge
 	
-You insert into tables with:
-	INSERT INTO table_name (column1, column2, column3, ...)
-	VALUES (value1, value2, value3, ...);
+you insert into tables with:
+	insert into table_name (column1, column2, column3, ...)
+	values (value1, value2, value3, ...);
 	
 or with
-	INSERT INTO table_name
-	VALUES (value1, value2, value3, ...);
+	insert into table_name
+	values (value1, value2, value3, ...);
 	
-If you want to delete, the code for that is something like:
-	DELETE FROM table 
-	WHERE condition;
+if you want to delete, the code for that is something like:
+	delete from table 
+	where condition;
 */
 
-INSERT INTO STORE
-VALUES ("1", "777 Lucky Street", "545692459"),
-	   ("2", "312 Some Address", "845692145");
+insert into store
+values ("1", "777 lucky street", "545692459"),
+	   ("2", "312 some address", "845692145");
 	   
-INSERT INTO EMPLOYEE
-VALUES ("John Smith", "1", "547 Rando Street","654125479","123456"),
-	   ("Jane Doe", "2", "327 Crazy Street","654125478","123456"),
-	   ("Ben Moskow", "3", "574 Ahhh Street","648254789","123456"),
-	   ("Sarah Stauber", "4", "764 Amazing Street","872145698","123456");
+insert into employee
+values ("john smith", "1", "547 rando street","654125479","123456"),
+	   ("jane doe", "2", "327 crazy street","654125478","123456"),
+	   ("ben moskow", "3", "574 ahhh street","648254789","123456"),
+	   ("sarah stauber", "4", "764 amazing street","872145698","123456");
 	   
 	   
 /*
-List of Movies to add to the DATABASE
-Information needed: ID, Title, Director, Producer, 
-	Actor1, Actor2, Category
-Batman Begins
-Great Gatsby
-Fight Club
-Titanic
-Pokemon 2000
-Wolf of Wall Street
-Ted 2
+list of movies to add to the database
+information needed: id, title, director, producer, 
+	actor1, actor2, category
+batman begins
+great gatsby
+fight club
+titanic
+pokemon 2000
+wolf of wall street
+ted 2
 */
 
-INSERT INTO MOVIE
-VALUES ("1", "Batman Begins", "Christopher Nolan", "Emma Thomas", "Christian Bale", "Katie Holmes", "Action"),
-	   ("2", "Great Gatsby", "Baz Luhrmann", "Baz Luhrmann", "Leonardo DiCaprio", "Carey Mulligan", "Romance"),
-	   ("3", "Fight Club", "David Fincher", "Art Linson", "Brad Pitt", "Edward Norton", "Action"),
-	   ("4", "Titanic", "James Cameron", "James Cameron", "Leonardo DiCaprio", "Kate Winslet", "Romance"),
-	   ("5", "Pokemon 2000", "Kunihiko Yuyama", "Choji Yoshikawa", "Ikue Otani", "Unsho Ishizuka", "Anime"),
-	   ("6", "Wolf of Wall Street", "Martin Scorsese", "Leonardo DiCaprio", "Leonardo DiCaprio", "Margot Robbie", "Comedy");
+insert into movie
+values ("1", "batman begins", "christopher nolan", "emma thomas", "christian bale", "katie holmes", "action"),
+	   ("2", "great gatsby", "baz luhrmann", "baz luhrmann", "leonardo dicaprio", "carey mulligan", "romance"),
+	   ("3", "fight club", "david fincher", "art linson", "brad pitt", "edward norton", "action"),
+	   ("4", "titanic", "james cameron", "james cameron", "leonardo dicaprio", "kate winslet", "romance"),
+	   ("5", "pokemon 2000", "kunihiko yuyama", "choji yoshikawa", "ikue otani", "unsho ishizuka", "anime"),
+	   ("6", "wolf of wall street", "martin scorsese", "leonardo dicaprio", "leonardo dicaprio", "margot robbie", "comedy");
 
-INSERT INTO MEMBER
-VALUES ("123456", "514 Happy Address", "Ryan Zunker","password1"),
-	   ("234567", "845 Other Street", "Maggi Lopez","maggi123"),
-	   ("345678", "215 Name of Street", "Leo Carrico","mypassword");
+insert into member
+values ("123456", "514 happy address", "ryan zunker","password1"),
+	   ("234567", "845 other street", "maggi lopez","maggi123"),
+	   ("345678", "215 name of street", "leo carrico","mypassword");
 	   
 	   
 /*
 	options for type are: 
-		Blu-ray
-		DVD
+		blu-ray
+		dvd
 	options for status are:
-		In-Store
-		Checked Out
+		in-store
+		checked out
 */
-INSERT INTO COPY
-VALUES ("1", "Blu-ray", "In-Store", "1"),
-	   ("2", "Blu-ray", "In-Store", "1"),
-	   ("3", "DVD", "In-Store", "1"),
-	   ("4", "DVD", "In-Store", "1"),
-	   ("5", "Blu-ray", "In-Store", "2"),
-	   ("6", "Blu-ray", "In-Store", "2"),
-	   ("7", "DVD", "In-Store", "2"),
-	   ("8", "DVD", "In-Store", "2"),
-	   ("9", "Blu-ray", "In-Store", "3"),
-	   ("10", "Blu-ray", "In-Store", "3"),
-	   ("11", "DVD", "In-Store", "3"),
-	   ("12", "DVD", "In-Store", "3"),
-	   ("13", "Blu-ray", "In-Store", "4"),
-	   ("14", "Blu-ray", "In-Store", "4"),
-	   ("15", "DVD", "In-Store", "4"),
-	   ("16", "DVD", "In-Store", "4"),
-	   ("17", "Blu-ray", "In-Store", "5"),
-	   ("18", "Blu-ray", "In-Store", "5"),
-	   ("19", "DVD", "In-Store", "5"),
-	   ("20", "DVD", "In-Store", "5"),
-	   ("21", "Blu-ray", "In-Store", "6"),
-	   ("22", "Blu-ray", "In-Store", "6"),
-	   ("23", "DVD", "In-Store", "6"),
-	   ("24", "DVD", "In-Store", "6"),
-	   ("25", "Blu-ray", "In-Store", "6"),
-	   ("26", "Blu-ray", "In-Store", "6"),
-	   ("27", "DVD", "In-Store", "6"),
-	   ("28", "DVD", "In-Store", "6");
+insert into copy
+values ("1", "blu-ray", "in-store", "1"),
+	   ("2", "blu-ray", "in-store", "1"),
+	   ("3", "dvd", "in-store", "1"),
+	   ("4", "dvd", "in-store", "1"),
+	   ("5", "blu-ray", "in-store", "2"),
+	   ("6", "blu-ray", "in-store", "2"),
+	   ("7", "dvd", "in-store", "2"),
+	   ("8", "dvd", "in-store", "2"),
+	   ("9", "blu-ray", "in-store", "3"),
+	   ("10", "blu-ray", "in-store", "3"),
+	   ("11", "dvd", "in-store", "3"),
+	   ("12", "dvd", "in-store", "3"),
+	   ("13", "blu-ray", "in-store", "4"),
+	   ("14", "blu-ray", "in-store", "4"),
+	   ("15", "dvd", "in-store", "4"),
+	   ("16", "dvd", "in-store", "4"),
+	   ("17", "blu-ray", "in-store", "5"),
+	   ("18", "blu-ray", "in-store", "5"),
+	   ("19", "dvd", "in-store", "5"),
+	   ("20", "dvd", "in-store", "5"),
+	   ("21", "blu-ray", "in-store", "6"),
+	   ("22", "blu-ray", "in-store", "6"),
+	   ("23", "dvd", "in-store", "6"),
+	   ("24", "dvd", "in-store", "6"),
+	   ("25", "blu-ray", "in-store", "6"),
+	   ("26", "blu-ray", "in-store", "6"),
+	   ("27", "dvd", "in-store", "6"),
+	   ("28", "dvd", "in-store", "6");
 	   
 	   
-INSERT INTO PLAYER
-VALUES ("1", "DP132", "LG", "Multi-Format"),
-	   ("2", "UBPX1000EX", "Sony", "4K Ultra HD");
+insert into player
+values ("1", "dp132", "lg", "multi-format"),
+	   ("2", "ubpx1000ex", "sony", "4k ultra hd");
 
 /*
-	Invoice_transaction
+	invoice_transaction
 	
-	The insert format for datetime looks like this:
-	INSERT INTO t1 VALUES ("2011-03-11"), ("2012-04-19 13:08:22"),
+	the insert format for datetime looks like this:
+	insert into t1 values ("2011-03-11"), ("2012-04-19 13:08:22"),
 						  ("2013-07-18 13:44:22.123456");
 						  
-	Types are:
-		Checkout
-		Fine
+	types are:
+		checkout
+		fine
 	
-	For the amount, I did $1 per day, so $7. 
-		I'm not dealing with decimals or change.
+	for the amount, i did $1 per day, so $7. 
+		i'm not dealing with decimals or change.
 */
 
-INSERT INTO INVOICE_TRANSACTION
-VALUES ("1", "2018-12-01", 7, "Checkout", "1", "3", "123456"),
-	   ("2", "2018-12-05", 3, "Fine", "1", "3", "123456"),
-	   ("3", "2018-12-10", 7, "Checkout", "2", "12", "234567");
+insert into invoice_transaction
+values ("1", "2018-12-01", 7, "checkout", "1", "3", "123456"),
+	   ("2", "2018-12-05", 3, "fine", "1", "3", "123456"),
+	   ("3", "2018-12-10", 7, "checkout", "2", "12", "234567");
 
-INSERT INTO `HWORKS` (`EMPLOYEEID`, `RATE`, `HOURS`, `STORENO`) VALUES ('1', '15', '30', '1');
-INSERT INTO `HWORKS` (`EMPLOYEEID`, `RATE`, `HOURS`, `STORENO`) VALUES ('2', '15', '30', '2');
-INSERT INTO `FULL_TIME` (`EMPLOYEEID`, `SALARY`, `STORENO`) VALUES ('3', '12', '1');
-INSERT INTO `FULL_TIME` (`EMPLOYEEID`, `SALARY`, `STORENO`) VALUES ('4', '15', '2');
+insert into `hworks` (`employeeid`, `rate`, `hours`, `storeno`) values ('1', '15', '30', '1');
+insert into `hworks` (`employeeid`, `rate`, `hours`, `storeno`) values ('2', '15', '30', '2');
+insert into `full_time` (`employeeid`, `salary`, `storeno`) values ('3', '12', '1');
+insert into `full_time` (`employeeid`, `salary`, `storeno`) values ('4', '15', '2');
 
-INSERT INTO `PLAYER_DEVICE` (`COPYNO`, `STORENO`, `PLAYERID`) VALUES ('1', '1', '1');
-INSERT INTO `PLAYER_DEVICE` (`COPYNO`, `STORENO`, `PLAYERID`) VALUES ('2', '2', '2');
-INSERT INTO `PLAYER_DEVICE` (`COPYNO`, `STORENO`, `PLAYERID`) VALUES ('3', '2', '1');
+insert into `player_device` (`copyno`, `storeno`, `playerid`) values ('1', '1', '1');
+insert into `player_device` (`copyno`, `storeno`, `playerid`) values ('2', '2', '2');
+insert into `player_device` (`copyno`, `storeno`, `playerid`) values ('3', '2', '1');
 
-INSERT INTO `STORE_CHARGE` (`CHARGEID`, `DAILYCHARGE`, `COPYNO`) VALUES ('1', '10', '1');
-INSERT INTO `STORE_CHARGE` (`CHARGEID`, `DAILYCHARGE`, `COPYNO`) VALUES ('2', '13', '2');
-INSERT INTO `STORE_CHARGE` (`CHARGEID`, `DAILYCHARGE`, `COPYNO`) VALUES ('3', '15', '3');
+insert into `store_charge` (`chargeid`, `dailycharge`, `copyno`) values ('1', '10', '1');
+insert into `store_charge` (`chargeid`, `dailycharge`, `copyno`) values ('2', '13', '2');
+insert into `store_charge` (`chargeid`, `dailycharge`, `copyno`) values ('3', '15', '3');
