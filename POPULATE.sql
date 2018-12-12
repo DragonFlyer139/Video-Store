d8ea8203bf0874919b4152c83a350701bef4824f
lcarrico <lcarrico827@gmail.com>
2018-12-12 02:20:35 -0600

    SQL Populate File

---
+++ POPULATE.sql
@@ -0,0 +1,148 @@
+/*
+Not sure if I mentioned this before, but I'm using MariaDB
+
+Order of tables to populate:
+	STORE
+	EMPLOYEE
+	MOVIE
+	MEMBER
+	COPY
+	PLAYER
+	INVOICE_TRANSACTIONS
+		Typically these are created when we actually have invoices
+		But we're going to need an invoice history to test
+	NO_ASSIGNED
+	HWORKS
+	FULL_TIME
+	PLAYER_DEVICE
+	STORE_CHARGE
+	
+You insert into tables with:
+	INSERT INTO table_name (column1, column2, column3, ...)
+	VALUES (value1, value2, value3, ...);
+	
+or with
+	INSERT INTO table_name
+	VALUES (value1, value2, value3, ...);
+	
+If you want to delete, the code for that is something like:
+	DELETE FROM table 
+	WHERE condition;
+*/
+
+INSERT INTO store
+VALUES ("1", "777 Lucky Street", "545692459"),
+	   ("2", "312 Some Address", "845692145");
+	   
+INSERT INTO employee
+VALUES ("John Smith", "1", "547 Rando Street","654125479","123456"),
+	   ("Jane Doe", "2", "327 Crazy Street","654125478","123456"),
+	   ("Ben Moskow", "3", "574 Ahhh Street","648254789","123456"),
+	   ("Sarah Stauber", "4", "764 Amazing Street","872145698","123456");
+	   
+	   
+/*
+List of Movies to add to the DATABASE
+Information needed: ID, Title, Director, Producer, 
+	Actor1, Actor2, Category
+
+Batman Begins
+Great Gatsby
+Fight Club
+Titanic
+Pokemon 2000
+Wolf of Wall Street
+Ted 2
+*/
+
+INSERT INTO movie
+VALUES ("1", "Batman Begins", "Christopher Nolan", "Emma Thomas", "Christian Bale", "Katie Holmes", "Action"),
+	   ("2", "Great Gatsby", "Baz Luhrmann", "Baz Luhrmann", "Leonardo DiCaprio", "Carey Mulligan", "Romance"),
+	   ("3", "Fight Club", "David Fincher", "Art Linson", "Brad Pitt", "Edward Norton", "Action"),
+	   ("4", "Titanic", "James Cameron", "James Cameron", "Leonardo DiCaprio", "Kate Winslet", "Romance"),
+	   ("5", "Pokemon 2000", "Kunihiko Yuyama", "Choji Yoshikawa", "Ikue Otani", "Unsho Ishizuka", "Anime"),
+	   ("6", "Wolf of Wall Street", "Martin Scorsese", "Leonardo DiCaprio", "Leonardo DiCaprio", "Margot Robbie", "Comedy"),
+	   ("7", "Ted 2", "Martin Scorsese", "Seth MacFarlane", "Seth MacFarlane", "Seth MacFarlane", "Mark Wahlberg", "Comedy");
+
+INSERT INTO member
+VALUES ("123456", "514 Happy Address", "Ryan Zunker"),
+	   ("234567", "845 Other Street", "Maggi Lopez"),
+	   ("345678", "215 Name of Street", "Leo Carrico");
+	   
+	   
+/*
+	options for type are: 
+		Blu-ray
+		DVD
+	options for status are:
+		In-Store
+		Checked Out
+*/
+INSERT INTO copy
+VALUES ("1", "Blu-ray", "In-Store", "1"),
+	   ("2", "Blu-ray", "In-Store", "1"),
+	   ("3", "DVD", "In-Store", "1"),
+	   ("4", "DVD", "In-Store", "1"),
+	   ("5", "Blu-ray", "In-Store", "2"),
+	   ("6", "Blu-ray", "In-Store", "2"),
+	   ("7", "DVD", "In-Store", "2"),
+	   ("8", "DVD", "In-Store", "2"),
+	   ("9", "Blu-ray", "In-Store", "3"),
+	   ("10", "Blu-ray", "In-Store", "3"),
+	   ("11", "DVD", "In-Store", "3"),
+	   ("12", "DVD", "In-Store", "3"),
+	   ("13", "Blu-ray", "In-Store", "4"),
+	   ("14", "Blu-ray", "In-Store", "4"),
+	   ("15", "DVD", "In-Store", "4"),
+	   ("16", "DVD", "In-Store", "4"),
+	   ("17", "Blu-ray", "In-Store", "5"),
+	   ("18", "Blu-ray", "In-Store", "5"),
+	   ("19", "DVD", "In-Store", "5"),
+	   ("20", "DVD", "In-Store", "5"),
+	   ("21", "Blu-ray", "In-Store", "6"),
+	   ("22", "Blu-ray", "In-Store", "6"),
+	   ("23", "DVD", "In-Store", "6"),
+	   ("24", "DVD", "In-Store", "6"),
+	   ("25", "Blu-ray", "In-Store", "7"),
+	   ("26", "Blu-ray", "In-Store", "7"),
+	   ("27", "DVD", "In-Store", "7"),
+	   ("28", "DVD", "In-Store", "7");
+	   
+	   
+INSERT INTO player
+VALUES ("1", "DP132", "LG", "Multi-Format"),
+	   ("2", "UBPX1000EX", "Sony", "4K Ultra HD");
+
+/*
+	Invoice_transaction
+	
+	The insert format for datetime looks like this:
+	INSERT INTO t1 VALUES ("2011-03-11"), ("2012-04-19 13:08:22"),
+						  ("2013-07-18 13:44:22.123456");
+						  
+	Types are:
+		Checkout
+		Fine
+	
+	For the amount, I did $1 per day, so $7. 
+		I'm not dealing with decimals or change.
+*/
+
+INSERT INTO invoice_transaction
+VALUES ("1", "2018-12-01", 7, "Checkout", "1", "3", "123456"),
+	   ("2", "2018-12-05", 3, "Fine", "1", "3", "123456"),
+	   ("3", "2018-12-10", 7, "Checkout", "2", "12", "234567");
+	
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
