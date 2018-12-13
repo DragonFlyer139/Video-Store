c5bd3570685288ff7f7f7873391c51453021cf41
lcarrico <lcarrico827@gmail.com>
2018-12-13 11:19:01 -0600

    Implemented Password

--- member_form_target.php
+++ member_form_target.php
@@ -18,20 +18,23 @@
   {
     die("Connection failed: " . $conn->connect_error);
   }
-  echo "Connected successfully<br>";
   
   
-  $id = isset($_POST["memberid"]) ? $_POST["memberid"] : "ID NOT FOUND";
+    $id = isset($_POST["memberid"]) ? $_POST["memberid"] : "ID NOT FOUND";
+	$password = isset($_POST["password"]) ? $_POST["password"] : "PASSWORD NOT FOUND";
+	
 	$_SESSION["memberid"] = $id;
 	
-	echo "ID NUMBER: " . $id . "<br>";
 	
     $sql = "select membername from member where memberid='" . $id ."'";
-	echo $sql . "<br>";
+	$sql .= " and " . "password='" . $password . "';";
     $result = $conn->query($sql);
 	
-    if ($result->num_rows == 0) 
-      header("Location: error.html");
+	
+    if (null == $result or $result->num_rows == 0){
+		echo "Card number and/or password incorrect. Please go back and try again." . "<br>";
+		echo "<a href=\"member_login.php\">Member Login</a>";
+	}
     else
     {
       $row = $result->fetch_assoc();
