2c708a46c5951cf223012c0b2f635ce65450214b
Emma Juettner <22248856+emmajuettner@users.noreply.github.com>
2018-12-14 05:44:41 -0600

    Add basic styles to all pages except search results

--- add_customer.php
+++ add_customer.php
@@ -1,10 +1,31 @@
 <html>
 <head><title>Add Customer</title>
+<!--STYLES STUFF START-->
+<meta charset = "UTF-8">
+	<meta keyword name = "viewport" content = "width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="img/icon.png">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+	<script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script> -->
+<!--STYLES STUFF END-->
 </head>
 <body>
 
+
+<!--STYLES STUFF START-->
+<div class="modal-dialog text-center">
+		<div class="main-section">
+			<div class="modal-content">
+<!--STYLES STUFF END-->
+
 <h3>Enter Customer Information</h3>
 <br>
+
+
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
   First Name: <input type="text" name = "FirstName"><br>
   Last Name: <input type="text" name = "LastName"><br>
@@ -12,7 +33,7 @@
 	Password: <input type = "password" name = "Password"><br>
 	Re-enter Password: <input type = "password" name = "Password2"><br>
 	
-	<input type = "Submit" value = "Sign Up">
+	<button type = "Submit" value = "Sign Up" class="btn">SIGN UP</button>
 </form>
 <br>
 <?php
@@ -54,7 +75,14 @@
   }
   
   
-?>
-<a href="admin_menu.php">Back</a>
+?><a href="admin_menu.php">Back</a>
+
+<!--STYLES STUFF START-->
+</div>
+		</div>
+	</div>
+<!--STYLES STUFF END-->
+
+
 </body>
 </html>
